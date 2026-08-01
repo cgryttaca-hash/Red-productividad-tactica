c328a1dd24a41832d6c5f6f2ef94365ed5287130
cgryttaca-hash <cgryttaca@gmail.com>
2026-08-01 00:38:46 -0500

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+export const FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyDHLGK-gODEXTtqC1EfSTEm2Hp7HlzF92M",
+  authDomain: "red-productividad-tactica.firebaseapp.com",
+  projectId: "red-productividad-tactica",
+  storageBucket: "red-productividad-tactica.firebasestorage.app",
+  messagingSenderId: "866011817933",
+  appId: "1:866011817933:web:c5ebf9f13652c58598c684"
+});
+export const FIREBASE_OWNER_UID = "Nxsbigjez6eBRMiZZdTzHv3oINn1";
+export const EVENTS_COLLECTION = "eventos_publicos";
+export const META_COLLECTION = "eventos_meta";
+export const CHANGES_COLLECTION = "eventos_cambios";
